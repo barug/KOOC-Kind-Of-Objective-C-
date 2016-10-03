8f1fc82334c29d022779583801945e12618d60c7
burges_q <git@git.epitech.eu>
2016-10-03 11:53:11 +0200

    test.h v1

---
+++ tools/Traduction/Test.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H_
+# define TEST_H_
+
+int	_K4Test1fER3intA2_3int4char(int, char);
+char	_K4Test1fER4charA2_3int4char(int, char);
+void	_K4Test1fER4voidA2_3intP4char(int*, char);
+
+int	_K4Test1aET3int = 42;
+float	_K4Test1aET4float = 42.42;
+char*	_K4Test1aET4charP = "42";
+
+# endif	 /* TEST_H */
