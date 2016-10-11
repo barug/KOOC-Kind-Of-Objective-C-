f163ca5fce67096429ed9e30514163cd37344fd7
Bogard <thomas.bogard@epitech.eu>
2016-10-11 14:40:31 +0200

    examples for kooc langage

---
+++ design_documents/examples/t_class.c
@@ -0,0 +1,11 @@
+typedef struct	t_Test
+{
+  int		_K4Test1aET3int;
+  int		_K4Test6memberET3int;
+}		Test;
+
+int		_K4Test9nonmemberET3int;
+void		_K4Test19myNonMemberFunctionER4voidA0_();
+void		_K4Test11mb_functionER4voidA0_();
+void		_K4Test21myOtherMemberFunctionER4voidA0_();
+void		_K4Test22myOtherMemberFunction2ER4voidA1_P4Test(Test *);
