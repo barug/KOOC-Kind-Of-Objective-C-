2c280be3b25dfaea077b4c516c7f1b37469edf2b
Barthélemy Gouby <barthelemy.gouby@gmail.com>
2016-10-11 17:08:43 +0200

    adding translate polymorphic method

--- design_documents/WORK/koocClasses.py
+++ design_documents/WORK/koocClasses.py
@@ -1,24 +1,39 @@
 from pyrser.parsing.node import Node
 
 class KoocNode(Node):
+    def translate(self):
+        pass
     pass
 
 class KoocDeclaration(KoocNode):
+    def translate(self):
+        pass
     pass
 
 class KoocStatement(KoocNode):
+    def translate(self):
+        pass
     pass
 
 class moduleDeclaration(KoocDeclaration):
     def __init__(self, moduleName, compoundDeclaration):
         self.moduleName = moduleName
         self.compoundDeclaration = compoundDeclaration
+
+    def translate(self):
+        pass                    # code goes here
         
 class moduleImplementation(KoocDeclaration):
     def __init__(self, moduleName, compoundDeclaration):
         self.moduleName = moduleName
         self.compoundDeclaration = compoundDeclaration
 
+    def translate(self):
+        pass                    # code goes here
+
 class moduleImport(KoocStatement):
     def __init__(self, moduleName):
         self.moduleName = moduleName
+
+    def translate(self):
+        pass                    # code goes here
