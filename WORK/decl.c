ff63b996648e894c373cd4bcf5df79ec41e7435b
Barthélemy Gouby <barthelemy.gouby@gmail.com>
2016-10-17 14:02:06 +0200

    working on ast translator

--- WORK/decl.c
+++ WORK/decl.c
@@ -1,6 +1,11 @@
 
-int a;
-void f();
-
-a = 10;
-a = 10 * 10;
+/* int a; */
+/* int *b; */
+/* int **c; */
+/* int d[]; */
+/* int *e[]; */
+/* int f[][]; */
+/* void e(); */
+/* void f(int a, char b); */
+void (*e)();
+void (*f)(int a, int b);
