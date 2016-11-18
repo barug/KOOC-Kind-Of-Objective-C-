70ab5a97a5977ddc758de4301e41ec8dd833065b
barthelemy gouby <barthelemy.gouby@gmail.com>
2016-11-18 17:59:58 +0100

    class struct implemented

--- WORK/decl.c
+++ WORK/decl.c
@@ -1,11 +1,29 @@
 
 /* int a; */
 /* int *b; */
-/* int **c; */
+int **c;
 /* int d[]; */
 /* int *e[]; */
 /* int f[][]; */
 /* void e(); */
-/* void f(int a, char b); */
-void (*e)();
-void (*f)(int a, int b);
+void f(void *a, char b);
+/* void (*e)(); */
+/* void (*f)(int a, int b); */
+
+/* truct aaa { */
+/*   int wesh; */
+/*   char test; */
+/*   void *a; */
+/* }; */
+
+/* typedef struct lol { */
+/*   int wesh; */
+/*   char test; */
+/*   void *a; */
+/* } lol; */
+
+/* void main() */
+/* { */
+/* } */
+
+/* typedef struct lol relol; */
