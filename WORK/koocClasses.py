e02397d847dbef565385a45f36e526a8026282dc
burges_q <git@git.epitech.eu>
2016-11-19 18:47:02 +0100

    patch var for KoocClassFunc otw

--- WORK/koocClasses.py
+++ WORK/koocClasses.py
@@ -80,11 +80,14 @@ class FunctionCall(KoocStatement) :
         self.Kclass = Kclass
         self.func = func
         self.argv = []
-        var = var.split()
+        print (var)
+        var = var.split(':')
         for arg in var :
-            pars = arg.split(')')
-            pars[0] = pars[0][2:]
-            self.argv.append(pars)
+            if (arg != '') :
+                print ('[' + arg + ']')
+            # pars = arg.split(')')
+            # pars[0] = pars[0][1:]
+                self.argv.append(arg)
 
     def translate(self):
         pass
